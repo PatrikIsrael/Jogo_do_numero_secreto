2b1697c4ea6949dfcecaddc2cef391266ff438e9
Patrik <patrik-israel@hotmail.com>
2024-02-26 18:03:01 -0300

    Primeiros passos

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function exibirTexto (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p','Escolha um número entre 1 e 10');
+
+function verificarChute() {
+   
+}
