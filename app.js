aebb0448e20abddbf055e60bfe74c7f1ff11aad1
Patrik <patrik-israel@hotmail.com>
2024-02-27 19:55:30 -0300

    foi adicionado funcao reiniciar jogo junto com gerarNumero

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-let numeroSecreto = 2;
+let listaDeNumero = [];
+let numeroDisponiveis = 10;
+let numeroSecreto = gerarNumero();
+let tentativas = 1;
 
 function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+function mensagemInicial() {
+    exibirTexto('h1', 'Jogo do número secreto');
+    exibirTexto('p','Escolha um número entre 1 e 10');
+    
+}
+    mensagemInicial();
 
-exibirTexto('h1', 'Jogo do número secreto');
-exibirTexto('p','Escolha um número entre 1 e 10');
 
 function exibirImagem(id) {
-    document.getElementById('imagemFeliz').style.display = 'none';
+    document.getElementById('vamosComecar').style.display = 'none';
     document.getElementById('gifAcertou').style.display = 'none';
     document.getElementById('gifErrou').style.display = 'none';
 
-    if (id === 'imagemFeliz') {
-        document.getElementById('imagemFeliz').style.display = 'block';
+    if (id === 'vamosComecar') {
+        document.getElementById('vamosComecar').style.display = 'block';
     }
 
     document.getElementById(id).style.display = 'block';
@@ -25,38 +32,62 @@ function verificarChute() {
 
     if (chute == numeroSecreto) {
         exibirTexto('h1', 'Você acertou!');
-        exibirTexto('p', 'Parabéns, você acertou o número secreto');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+        let mensagemTentativas = `Voce acertou o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTexto('p', mensagemTentativas);
         exibirImagem('gifAcertou');
-        desabilitarInputEBotao();
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if(chute < numeroSecreto){
            exibirTexto('p','O número é menor que o número secreto');
         } else {
             exibirTexto('p','O número é maior que o número secreto');
         }
-        exibirTexto('h1', 'Você errou :(');
-        exibirImagem('gifErrou');
-    }
+            exibirTexto('h1', 'Você errou :(');
+            exibirImagem('gifErrou');
+            tentativas++;
+            limparCampo();
+    }       
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 }
 
 function gerarNumero() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroDisponiveis + 1);
+    let quantidadeElementosLista = listaDeNumero.length;
+
+    if(quantidadeElementosLista == numeroDisponiveis){
+        listaDeNumero = [];
+    }
+    if(listaDeNumero.includes(numeroEscolhido)){
+        return gerarNumero();
+    } else {
+        listaDeNumero.push(numeroEscolhido)
+        return numeroEscolhido;
+    }
+
 }
 
 function reiniciarJogo() {
-    numeroSecreto = 2;
-    exibirTexto('h1', 'Jogo do número secreto');
-    exibirTexto('p', 'Escolha um número entre 1 e 10');
+    numeroSecreto = gerarNumero();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
     document.querySelector('input').value = '';
     habilitarInputEBotao();
     ocultarTodasAsImagens();
+    document.getElementById('vamosComecar').style.display = 'none';
+
 }
 
 function ocultarTodasAsImagens() {
+    document.getElementById('vamosComecar').style.display = 'none';
     document.getElementById('gifAcertou').style.display = 'none';
     document.getElementById('gifErrou').style.display = 'none';
 }
-
 
 function desabilitarInputEBotao() {
     document.querySelector('input').disabled = true;
@@ -67,3 +98,4 @@ function habilitarInputEBotao() {
     document.querySelector('input').disabled = false;
     document.getElementById('reiniciar').disabled = true;
 }
+
